b3e65d7403d306f2913f6cdd36b2dbc07cadaff0
PhilipMay <philip@may.la>
2021-08-01 15:03:31 +0200

    add test

--- tests/test_lazy_imports.py
+++ tests/test_lazy_imports.py
@@ -11,3 +11,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from lazy_imports import LazyImporter, __version__
+
+
+def test_sinple_case():
+    _import_structure = {
+        "lazy_imports": ["LazyImporter"],
+    }
+    extra_objects = {"__version__": __version__}
+    lazy_importer = LazyImporter(
+        __name__,
+        globals()["__file__"],
+        _import_structure,
+        extra_objects=extra_objects,
+    )
+
+    assert lazy_importer._import_structure == _import_structure
+    assert lazy_importer._objects == extra_objects
+
+    lazy_importer_dir = dir(lazy_importer)
+
+    assert "lazy_imports" in lazy_importer_dir
+    assert "_import_structure" in lazy_importer_dir
+
+    assert lazy_importer.__version__ == __version__
