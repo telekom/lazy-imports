51aa9d784b2e9d7c05264023705386a6d163b58e
PhilipMay <philip@may.la>
2021-08-05 22:13:14 +0200

    bump version to 0.2.0

--- lazy_imports/__init__.py
+++ lazy_imports/__init__.py
@@ -20,6 +20,6 @@ from lazy_imports.try_import import try_import
 
 # Versioning follows the `Semantic Versioning Specification <https://semver.org/>`__ and
 # `PEP 440 -- Version Identification and Dependency Specification <https://www.python.org/dev/peps/pep-0440/>`__.  # noqa: E501
-__version__ = "0.1.1rc1"
+__version__ = "0.2.0"
 
 __all__ = ["LazyImporter", "try_import", "__version__"]
